bca66c13efb1b40cc272fcfb789de95346d7ab36
Barry Schwartz <150643+chemoelectric@users.noreply.github.com>
2026-08-12 07:41:43 -0500

    Start of Gauche binding.

--- public/iris-typesetting-suite/test_gauche_binding.adb
+++ public/iris-typesetting-suite/test_gauche_binding.adb
@@ -4,6 +4,7 @@ with gauche_binding; use gauche_binding;
 with interfaces.c; use interfaces.c;
 with interfaces.c.strings; use interfaces.c.strings;
 with ada.text_io; use ada.text_io;
+with ada.command_line; use ada.command_line;
 
 procedure test_gauche_binding is
    packet : aliased ScmEvalPacket;
@@ -16,4 +17,6 @@ begin
       env    => Scm_UserModule,
       packet => packet'access
    );
+
+   set_exit_status (if status < 0 then failure else success);
 end test_gauche_binding;
