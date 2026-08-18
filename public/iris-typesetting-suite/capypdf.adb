7c37a5ee42054d66bac1a3d40a390ba32fd3a163
Barry Schwartz <150643+chemoelectric@users.noreply.github.com>
2026-08-18 14:54:49 -0500

    Make capypdf.adb conform to project coding standards.

--- public/iris-typesetting-suite/capypdf.adb
+++ public/iris-typesetting-suite/capypdf.adb
@@ -45,23 +45,23 @@ package body capypdf is
    function close_document
      (doc : in out pdf_document) return capy_error
    is
-      retval : capy_error := capy_err_invalid_state;
+      capyerr : capy_error := capy_err_invalid_state;
    begin
       if doc.is_open then
-         retval := capy_generator_write (doc.gen);
-         if retval = capy_err_ok then
-            retval := capy_generator_destroy (doc.gen);
+         capyerr := capy_generator_write (doc.gen);
+         if capyerr = capy_err_ok then
+            capyerr := capy_generator_destroy (doc.gen);
          end if;
-         if retval = capy_err_ok then
-            retval := capy_document_properties_destroy (doc.opt);
+         if capyerr = capy_err_ok then
+            capyerr := capy_document_properties_destroy (doc.opt);
          end if;
-         if retval = capy_err_ok then
+         if capyerr = capy_err_ok then
             doc.gen     := null_generator;
             doc.opt     := null_options;
             doc.is_open := false;
          end if;
       end if;
-      return retval;
+      return capyerr;
    end close_document;
 
    function create_page_config
@@ -69,12 +69,12 @@ package body capypdf is
       height_pt : in interfaces.c.double) return page_config
    is
       config  : page_config;
-      res     : capy_error;
+      capyerr  : capy_error;
    begin
       config.width  := width_pt;
       config.height := height_pt;
-      res := capy_page_properties_new (config.prop);
-      if res /= capy_err_ok then
+      capyerr := capy_page_properties_new (config.prop);
+      if capyerr /= capy_err_ok then
          config.prop := null_page_properties;
       end if;
       return config;
@@ -83,10 +83,10 @@ package body capypdf is
    procedure destroy_page_config
      (config : in out page_config)
    is
-      res : capy_error;
+      capyerr: capy_error;
    begin
       if config.prop /= null_page_properties then
-         res := capy_page_properties_destroy (config.prop);
+         capyerr:= capy_page_properties_destroy (config.prop);
          config.prop := null_page_properties;
       end if;
    end destroy_page_config;
@@ -95,18 +95,17 @@ package body capypdf is
      (doc : in pdf_document) return pdf_draw_context
    is
       context : pdf_draw_context;
-      res     : capy_error;
+      capyerr  : capy_error;
    begin
       if not is_valid (doc) then
          context.active := false;
-         return context;
-      end if;
-
-      res := capy_page_draw_context_new (doc.gen, context.dc);
-      if res = capy_err_ok then
-         context.active := true;
       else
-         context.active := false;
+         capyerr := capy_page_draw_context_new (doc.gen, context.dc);
+         if capyerr = capy_err_ok then
+            context.active := true;
+         else
+            context.active := false;
+         end if;
       end if;
       return context;
    end create_draw_context;
@@ -118,31 +117,34 @@ package body capypdf is
       width  : in interfaces.c.double;
       height : in interfaces.c.double) return capy_error
    is
+      capyerr : capy_error := capy_err_invalid_state;
    begin
-      if not dc.active then
-         return capy_err_invalid_state;
+      if dc.active then
+         capyerr := capy_dc_cmd_re (dc.dc, x, y, width, height);
       end if;
-      return capy_dc_cmd_re (dc.dc, x, y, width, height);
+      return capyerr;
    end draw_rectangle;
 
    function fill_path
      (dc : in pdf_draw_context) return capy_error
    is
+      capyerr : capy_error := capy_err_invalid_state;
    begin
-      if not dc.active then
-         return capy_err_invalid_state;
+      if dc.active then
+         capyerr := capy_dc_cmd_f (dc.dc);
       end if;
-      return capy_dc_cmd_f (dc.dc);
+      return capyerr;
    end fill_path;
 
    function stroke_path
      (dc : in pdf_draw_context) return capy_error
    is
+      capyerr : capy_error := capy_err_invalid_state;
    begin
-      if not dc.active then
-         return capy_err_invalid_state;
+      if dc.active then
+         capyerr := capy_dc_cmd_s (dc.dc);
       end if;
-      return capy_dc_cmd_s (dc.dc);
+      return capyerr;
    end stroke_path;
 
    function set_stroke_rgb
@@ -151,11 +153,12 @@ package body capypdf is
       g  : in interfaces.c.double;
       b  : in interfaces.c.double) return capy_error
    is
+      capyerr : capy_error := capy_err_invalid_state;
    begin
-      if not dc.active then
-         return capy_err_invalid_state;
+      if dc.active then
+         capyerr :=  capy_dc_cmd_stroke_rg (dc.dc, r, g, b);
       end if;
-      return capy_dc_cmd_stroke_rg (dc.dc, r, g, b);
+      return capyerr;
    end set_stroke_rgb;
 
    function set_fill_rgb
@@ -164,64 +167,70 @@ package body capypdf is
       g  : in interfaces.c.double;
       b  : in interfaces.c.double) return capy_error
    is
+      capyerr : capy_error := capy_err_invalid_state;
    begin
-      if not dc.active then
-         return capy_err_invalid_state;
+      if dc.active then
+         capyerr :=  capy_dc_cmd_rg (dc.dc, r, g, b);
       end if;
-      return capy_dc_cmd_rg (dc.dc, r, g, b);
+      return capyerr;
    end set_fill_rgb;
 
    function set_stroke_gray
      (dc   : in pdf_draw_context;
       gray : in interfaces.c.double) return capy_error
    is
+      capyerr : capy_error := capy_err_invalid_state;
    begin
-      if not dc.active then
-         return capy_err_invalid_state;
+      if dc.active then
+         capyerr :=  capy_dc_cmd_stroke_g (dc.dc, gray);
       end if;
-      return capy_dc_cmd_stroke_g (dc.dc, gray);
+      return capyerr;
    end set_stroke_gray;
 
    function set_fill_gray
      (dc   : in pdf_draw_context;
       gray : in interfaces.c.double) return capy_error
    is
+      capyerr : capy_error := capy_err_invalid_state;
    begin
-      if not dc.active then
-         return capy_err_invalid_state;
+      if dc.active then
+         capyerr :=  capy_dc_cmd_g (dc.dc, gray);
       end if;
-      return capy_dc_cmd_g (dc.dc, gray);
+      return capyerr;
    end set_fill_gray;
 
    function set_line_width
      (dc : in pdf_draw_context;
       w  : in interfaces.c.double) return capy_error
    is
+      capyerr : capy_error := capy_err_invalid_state;
    begin
-      if not dc.active then
-         return capy_err_invalid_state;
+      if dc.active then
+         capyerr :=  capy_dc_cmd_w (dc.dc, w);
       end if;
-      return capy_dc_cmd_w (dc.dc, w);
+      return capyerr;
    end set_line_width;
 
    function save_state
      (dc : in pdf_draw_context) return capy_error
    is
+      capyerr : capy_error := capy_err_invalid_state;
    begin
-      if not dc.active then
-         return capy_err_invalid_state;
+      if dc.active then
+         capyerr := capy_dc_cmd_q (dc.dc);
       end if;
-      return capy_dc_cmd_q (dc.dc);
+      return capyerr;
    end save_state;
 
    function restore_state
      (dc : in pdf_draw_context) return capy_error
    is
+      capyerr : capy_error := capy_err_invalid_state;
    begin
-      if not dc.active then
-         return capy_err_invalid_state;
+      if dc.active then
+         capyerr := capy_dc_cmd_restore_q (dc.dc);
       end if;
-      return capy_dc_cmd_restore_q (dc.dc);
+      return capyerr;
    end restore_state;
 
    function load_font
@@ -230,19 +239,18 @@ package body capypdf is
       font      : out font_id) return capy_error
    is
       c_path : interfaces.c.strings.chars_ptr;
-      res    : capy_error;
+      capyerr   : capy_error := capy_err_invalid_state;
    begin
       if not is_valid (doc) then
          font := invalid_font_id;
-         return capy_err_invalid_state;
+      else
+         c_path := interfaces.c.strings.new_string (font_path);
+         capyerr:=
+           capy_generator_load_font
+             (doc.gen, c_path, null_font_properties, font);
+         interfaces.c.strings.free (c_path);
       end if;
-
-      c_path := interfaces.c.strings.new_string (font_path);
-      res    :=
-        capy_generator_load_font
-          (doc.gen, c_path, null_font_properties, font);
-      interfaces.c.strings.free (c_path);
-      return res;
+      return capyerr;
    end load_font;
 
    function render_text
@@ -254,42 +262,37 @@ package body capypdf is
       y         : in interfaces.c.double) return capy_error
    is
       c_str : interfaces.c.strings.chars_ptr;
-      res   : capy_error;
+      capyerr  : capy_error := capy_err_invalid_state;
    begin
-      if not dc.active then
-         return capy_err_invalid_state;
+      if dc.active then
+         c_str := interfaces.c.strings.new_string (text_str);
+         capyerr :=
+           capy_dc_render_text (dc.dc, c_str, font, font_size, x, y);
+         interfaces.c.strings.free (c_str);
       end if;
-
-      c_str := interfaces.c.strings.new_string (text_str);
-      res   :=
-        capy_dc_render_text (dc.dc, c_str, font, font_size, x, y);
-      interfaces.c.strings.free (c_str);
-      return res;
+      return capyerr;
    end render_text;
 
    function add_page_with_context
      (doc : in out pdf_document;
       dc  : in out pdf_draw_context) return capy_error
    is
-      add_res : capy_error;
-      dst_res : capy_error;
+      capyerr  : capy_error := capy_err_invalid_state;
    begin
       if not is_valid (doc) or else not dc.active then
-         return capy_err_invalid_state;
-      end if;
-
-      add_res := capy_generator_add_page (doc.gen, dc.dc);
-      dst_res := capy_dc_destroy (dc.dc);
-
-      dc.dc     := null_draw_context;
-      dc.active := false;
-
-      if add_res = capy_err_ok then
-         doc.pages := doc.pages + 1;
-         return dst_res;
+         null;
       else
-         return add_res;
+         capyerr := capy_generator_add_page (doc.gen, dc.dc);
+         if capyerr = capy_err_ok then
+            doc.pages := doc.pages + 1;
+            capyerr := capy_dc_destroy (dc.dc);
+         end if;
+         if capyerr = capy_err_ok then
+            dc.dc     := null_draw_context;
+            dc.active := false;
+         end if;
       end if;
+      return capyerr;
    end add_page_with_context;
 
 end capypdf;
