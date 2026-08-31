b4f713b676740b355941cb08407f08d51d7545c7
Barry Schwartz <150643+chemoelectric@users.noreply.github.com>
2026-08-31 04:22:25 -0500

    Add support for containers.

--- public/iris-typesetting-suite/sequential_identifiers.adb
+++ public/iris-typesetting-suite/sequential_identifiers.adb
@@ -5,9 +5,14 @@
 pragma wide_character_encoding (utf8);
 pragma ada_2022;
 
+with interfaces;
+with ada.containers;
 with system.atomic_operations.modular_arithmetic;
 
 package body sequential_identifiers is
+
+   use interfaces;
+   use ada.containers;
 
    type atomic_result_type is new sequential_identifier with atomic;
 
@@ -21,8 +26,68 @@ package body sequential_identifiers is
    function next_sequential_identifier return sequential_identifier is
       atomic_result : atomic_result_type;
    begin
-      atomic_result := atomic_fetch_and_add (item => counter, value => 1);
+      atomic_result :=
+        atomic_fetch_and_add (item => counter, value => 1);
       return sequential_identifier (atomic_result);
    end next_sequential_identifier;
+
+   function "<" (left, right : in sequential_identifier) return boolean
+   is
+   begin
+      return "<" (unsigned_64 (left), unsigned_64 (right));
+   end "<";
+
+   function ">" (left, right : in sequential_identifier) return boolean
+   is
+   begin
+      return ">" (unsigned_64 (left), unsigned_64 (right));
+   end ">";
+
+   function "=" (left, right : in sequential_identifier) return boolean
+   is
+   begin
+      return "=" (unsigned_64 (left), unsigned_64 (right));
+   end "=";
+
+   function "<=" (left, right : in sequential_identifier) return boolean
+   is
+   begin
+      return "<=" (unsigned_64 (left), unsigned_64 (right));
+   end "<=";
+
+   function ">=" (left, right : in sequential_identifier) return boolean
+   is
+   begin
+      return ">=" (unsigned_64 (left), unsigned_64 (right));
+   end ">=";
+
+   function hash_sequential_identifier
+     (key : in sequential_identifier) return hash_type
+   is
+      --
+      -- FIXME: USE SPOOKYHASH. IN FACT, USE SPOOKYHASH EVERYWHERE AND
+      -- ADD SUPPORT FOR INCREMENTAL HASHING.
+      --
+      -- Knuth’s method must be as old as the hills. It surely is not
+      -- bad, but we now have incremental hashing methods that can
+      -- deal better with arrays, lists, etc.
+      --
+
+      --
+      -- Knuth’s multiplier that (they say) does something such as use
+      -- the golden ratio to subdivide the space of hashes repeatedly
+      -- without running out of space. FIXME: LOOK UP THE REFERENCE IN
+      -- THE ART OF COMPUTER PROGRAMMING, AND FIX THIS COMMENT.
+      --
+      multiplier : constant sequential_identifier :=
+        11400714819323198485;
+
+      x : sequential_identifier;
+   begin
+      -- Use the multiplier, after mixing high and low bits.
+      x := multiplier * (shift_right (key, 30) xor key);
+      -- Mix high and low bits again.
+      return hash_type'mod (shift_right (x, 27) xor x);
+   end hash_sequential_identifier;
 
 end sequential_identifiers;
--- public/iris-typesetting-suite/sequential_identifiers.ads
+++ public/iris-typesetting-suite/sequential_identifiers.ads
@@ -6,11 +6,56 @@ pragma wide_character_encoding (utf8);
 pragma ada_2022;
 
 with interfaces;
+with ada.containers;
 
 package sequential_identifiers is
 
-   type sequential_identifier is new interfaces.unsigned_64;
+   type sequential_identifier is private;
 
    function next_sequential_identifier return sequential_identifier;
+
+   function "<" (left, right : in sequential_identifier) return boolean;
+
+   function ">" (left, right : in sequential_identifier) return boolean;
+
+   function "=" (left, right : in sequential_identifier) return boolean;
+
+   function "<="
+     (left, right : in sequential_identifier) return boolean;
+
+   function ">="
+     (left, right : in sequential_identifier) return boolean;
+
+   --
+   -- FIXME:
+   --
+   -- ADD SUPPORT EVERYWHERE FOR INCREMENTAL HASHING.
+   --
+   -- Use SpookyHash.
+   --
+   -- Ada seems not to do hashing terribly well, just as everyone else
+   -- seems not to do it well. Hashing should be done incrementally.
+   -- It is not taught this way in books, and so it is never learnt
+   -- this way, despite that if you look up new hashes on the Internet
+   -- for the last few decades you see only incremental ones.
+   --
+   -- The R⁷RS-large standard actually enshrines non-incremental
+   -- hashing, indirectly by standardizing a bad SRFI. It is a rare
+   -- part of R⁷RS that I reject. I use my own hash functions and hash
+   -- maps.
+   --
+   -- Our universities teach obedience of sacred writings and the
+   -- persecution of heretics. Count ourselves lucky that devising a
+   -- new algorithm does not land us in prison. I am the only person I
+   -- have ever seen using symmetric power basis, despite its enormous
+   -- advantages. Why? Because only Bernstein basis is taught in most
+   -- books as being ‘Bézier splines’.
+   --
+   function hash_sequential_identifier
+     (key : in sequential_identifier) return ada.containers.hash_type;
+
+private
+
+   type sequential_identifier is new interfaces.unsigned_64;
 
 end sequential_identifiers;
